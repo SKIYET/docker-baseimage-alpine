8e8a6a7d87bd33521fccf014335a863469bc30a6
SKIYET <49865295+SKIYET@users.noreply.github.com>
2025-09-02 21:27:54 +0800

    Update version-check.sh

--- .github/workflows/version-check.sh
+++ .github/workflows/version-check.sh
@@ -31,51 +31,51 @@ if [ -z "$OFFICIAL_VERSION" ]; then
     echo "::error::无法从 Docker Hub API 响应中解析出有效的 Alpine 版本标签。" >&2
     exit 1
 fi
-OFFICIAL_VERSION=3.21.3
+
 echo "官方 Alpine 最新版本: $OFFICIAL_VERSION" >&2
 
 # 获取我们镜像的最新版本
-#OUR_API_URL="https://hub.docker.com/v2/repositories/${DOCKER_IMAGE}/tags/?page_size=100&ordering=last_updated"
-#OUR_RESPONSE=$(curl -fsSL --retry 3 --retry-delay 2 "$OUR_API_URL" || echo '{"results":[]}')
+OUR_API_URL="https://hub.docker.com/v2/repositories/${DOCKER_IMAGE}/tags/?page_size=100&ordering=last_updated"
+OUR_RESPONSE=$(curl -fsSL --retry 3 --retry-delay 2 "$OUR_API_URL" || echo '{"results":[]}')
 
 # 提取我们镜像中符合 x.y.z 格式的最新版本
-#OUR_VERSION=$(echo "$OUR_RESPONSE" | jq -r '.results[].name' | grep -E '^[0-9]+(\.[0-9]+){1,2}$' | sort -Vr | head -n 1 || echo "0.0.0")
-OUR_VERSION=0.0.0
+OUR_VERSION=$(echo "$OUR_RESPONSE" | jq -r '.results[].name' | grep -E '^[0-9]+(\.[0-9]+){1,2}$' | sort -Vr | head -n 1 || echo "0.0.0")
+
 echo "我们的镜像最新版本: $OUR_VERSION" >&2
 
 # 版本比较函数
-#version_compare() {
-#    if [[ $1 == $2 ]]; then
-#        return 0  # 版本相等
-#    fi
-#    local IFS=.
-#    local i ver1=($1) ver2=($2)
-#    # 填充版本数组到相同长度
-#    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do
-#        ver1[i]=0
-#    done
-#    for ((i=${#ver2[@]}; i<${#ver1[@]}; i++)); do
-#        ver2[i]=0
-#    done
-#    # 逐个比较版本号组件
-#    for ((i=0; i<${#ver1[@]}; i++)); do
-#        if [[ -z ${ver2[i]} ]]; then
-#            ver2[i]=0
-#        fi
-#        if ((10#${ver1[i]} > 10#${ver2[i]})); then
-#            return 1  # 第一个版本更高
-#        fi
-#        if ((10#${ver1[i]} < 10#${ver2[i]})); then
-#            return 2  # 第二个版本更高
-#        fi
-#    done
-#    return 0  # 版本相等
-#}
+version_compare() {
+    if [[ $1 == $2 ]]; then
+        return 0  # 版本相等
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # 填充版本数组到相同长度
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do
+        ver1[i]=0
+    done
+    for ((i=${#ver2[@]}; i<${#ver1[@]}; i++)); do
+        ver2[i]=0
+    done
+    # 逐个比较版本号组件
+    for ((i=0; i<${#ver1[@]}; i++)); do
+        if [[ -z ${ver2[i]} ]]; then
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]})); then
+            return 1  # 第一个版本更高
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]})); then
+            return 2  # 第二个版本更高
+        fi
+    done
+    return 0  # 版本相等
+}
 
 # 比较版本
-#version_compare "$OFFICIAL_VERSION" "$OUR_VERSION"
-#comparison_result=$?
-comparison_result=1
+version_compare "$OFFICIAL_VERSION" "$OUR_VERSION"
+comparison_result=$?
+
 if [ $comparison_result -eq 1 ]; then
     echo "官方版本 ($OFFICIAL_VERSION) 比我们的版本 ($OUR_VERSION) 更高，需要构建新镜像" >&2
     SHOULD_BUILD="true"
